ad24bbe9cfca588ca0b671241e2c226997b8ad32
Divjot-Babra <82795103+Divjot-Babra@users.noreply.github.com>
2021-05-12 14:56:02 +1000

    Updated All Functions

--- Assignment_2.m
+++ Assignment_2.m
@@ -1,42 +1,31 @@
-%% Assignment 2 Program
+%% DUAL DOBOT GAMEPLAY - Assignment 2 Program
 % Divjot Babra, Nikhil Senthilvel, Vivien Thai
 clc % Clear command window
 clearvars % Clear workspace variables
 clf % Close all figures
 set(0,'DefaultFigureWindowStyle','docked') % Docking figure window
-%% Token Location Input
-
-P1 = transl(0.8,0.7,0.35);
-% P2 = transl(1.75,0.5,0.2);                   
-% P3 = transl(2,0.5,0.2);
-% P4 = transl(1.5,0.25,0.22);
-% P5 = transl(1.75,0.25,0.2);
-% P6 = transl(2,0.25,0.2);
-% P7 = transl(1.5,0.75,0.2);
-% P8 = transl(1.75,0.75,0.2);
-% P9 = transl(2.25,0.85,0.2);
-
-%% Plot Robots
-
-q0 = [0,0,0,0];
-
-dobot1 = dobot;
-
-dobot2 = dobot2;
-
+%% Token Starting Positions
+T1 = transl(0.8,0.7,0.35);
+% T2 = transl(1.75,0.5,0.2);                   
+% T3 = transl(2,0.5,0.2);
+% T4 = transl(1.5,0.25,0.22);
+% T5 = transl(1.75,0.25,0.2);
+% T6 = transl(2,0.25,0.2);
+% T7 = transl(1.5,0.75,0.2);
+% T8 = transl(1.75,0.75,0.2);
+% T9 = transl(2.25,0.85,0.2);
+%% Plot Both Dobots
+q0 = [0,0,0,0,0];
+dobot1 = Dobot1;
+hold on;
+dobot2 = Dobot2;
 %% Create Environment
-
-Environment()
-
-%% Create Floor and Walls
-
-
+Environment(); % Call environment function
 %% Plotting Token
 workspace = [0 2 0 2 0 0.8];     
 scale = 0.5;
 
 L1 = Link('d',0,'a',0,'alpha',pi/2,'qlim',deg2rad([-360,360]), 'offset', 0);     % DH parameters of UR3 robot
-
 
 [faceData,vertexData] = plyread('Token O.ply','tri');
 Token = SerialLink([L1],'name','Token','base',P1);     % Inputting 9 bricks     
@@ -44,9 +33,6 @@ Token.faces = {faceData,[]};                           % Referring to robotcows.
 Token.points = {vertexData,[]};                        % Inputting brick faces and vertices
 qa = zeros(1,1);                                       % Creating initial joint angles as zero for all bricks
 Token.plot3d(qa,'workspace',workspace,'scale',scale);
-
-
-
 
 %% Moving Dobot to Token
 % 
--- Environment.m
+++ Environment.m
@@ -1,96 +1,61 @@
-function [input] = Environment(output)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-[f,v,data] = plyread('Table.ply','tri');      % Inputting environment ply
-                                                    % file from Blender
-
-vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; %rgb colour
-
-hold on;                                            % Ensures environment stays
-
-for zOffset = [0]                                   % Positioned environment at the origin
-    for yOffset = [0]
-        for xOffset = [0]
-        modelcoordinate = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
-        ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+function [ ] = Environment() % Function for creating simulation environment
+%% Inserting Objects
+[f,v,data] = plyread('Table.ply','tri'); % Inserting PLY object
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; % Scaling colours
+hold on; % Hold figure
+for zOffset = (0) % Position object in environment
+    for yOffset = (0)
+        for xOffset = (0)
+        trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset, ...
+        'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
 end
+axis equal; % Set axis to be equal        
+hold on; % Hold figure
 
-axis equal;                                         
-hold on;
-
-
-
-[f,v,data] = plyread('Fire Extinguisher.ply','tri');      % Inputting environment ply
-                                                    % file from Blender
-
-vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; %rgb colour
-
-hold on;                                            % Ensures environment stays
-
-for zOffset = [0]                                   % Positioned environment at the origin
-    for yOffset = [0]
-        for xOffset = [0]
-        modelcoordinate = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
-        ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+[f,v,data] = plyread('Fire Extinguisher.ply','tri'); % Inserting PLY object
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; % Scaling colours
+hold on; % Hold figure
+for zOffset = (0) % Position object in environment
+    for yOffset = (0)
+        for xOffset = (0)
+        trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset, ...
+        'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
 end
+axis equal; % Set axis to be equal        
+hold on; % Hold figure
 
-axis equal;                                         
-hold on;
-
-
-
-
-[f,v,data] = plyread('E-Stop.ply','tri');      % Inputting environment ply
-                                                    % file from Blender
-
-vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; %rgb colour
-
-hold on;                                            % Ensures environment stays
-
-for zOffset = [0]                                   % Positioned environment at the origin
-    for yOffset = [0]
-        for xOffset = [0]
-        modelcoordinate = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
-        ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+[f,v,data] = plyread('E-Stop.ply','tri'); % Inserting PLY object
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; % Scaling colours
+hold on; % Hold figure
+for zOffset = (0) % Position object in environment
+    for yOffset = (0)
+        for xOffset = (0)
+        trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset, ...
+        'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
 end
+axis equal; % Set axis to be equal        
+hold on; % Hold figure
 
-axis equal;                                         
-hold on;
-
-
-
-
-
-[f,v,data] = plyread('Board.ply','tri');      % Inputting environment ply
-                                                    % file from Blender
-
-vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; %rgb colour
-
-hold on;                                            % Ensures environment stays
-
-for zOffset = [0]                                   % Positioned environment at the origin
-    for yOffset = [0]
-        for xOffset = [0]
-        modelcoordinate = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
-        ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+[f,v,data] = plyread('Board.ply','tri'); % Inserting PLY object
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255; % Scaling colours
+hold on; % Hold figure
+for zOffset = (0) % Position object in environment
+    for yOffset = (0)
+        for xOffset = (0)
+        trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset, ...
+        'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
 end
-
-axis equal;                                         
-hold on;
-
-
-
-
-
+axis equal; % Set axis to be equal        
+hold on; % Hold figure
+%% Inserting Floor and Walls
 surf([0,0;2,2],[0,2;0,2],[0.05,0.05;0.05,0.05],'CData',imread('WoodFloor.jpg'),'FaceColor','texturemap')
 % surf([-1.2,-1.2;3.2,3.2],[-1.2,1.2;-1.2,1.2],[0.01,0.01;0.01,0.01],'CData',imread('tape.jpg'),'FaceColor','texturemap')
 % surf([-0.8,-0.8;2.8,2.8],[-0.8,0.8;-0.8,0.8],[0.01,0.01;0.01,0.01],'CData',imread('concrete.jpg'),'FaceColor','texturemap')
@@ -99,28 +64,4 @@ surf([0,0;2,2],[0,2;0,2],[0.05,0.05;0.05,0.05],'CData',imread('WoodFloor.jpg'),'
 surf([2,2;2,2],[2,2;0,0],[1,0;1,0],'CData',imread('RightWall.jpg'),'FaceColor','texturemap')
 surf([2,2;0,0],[2,2;2,2],[1,0;1,0],'CData',imread('Window.jpg'),'FaceColor','texturemap')
 % surf([-4,-4;-4,-4],[4,4;-2,-2],[2,0.01;2,0.01],'CData',imread('TicTacToeBackground.jpg'),'FaceColor','texturemap')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
-
