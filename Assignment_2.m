82d215ec04e135a7b209a41585ce56d675dbf065
Divjot-Babra <82795103+Divjot-Babra@users.noreply.github.com>
2021-05-12 20:13:20 +1000

    Added code for token placing

--- Assignment_2.m
+++ Assignment_2.m
@@ -5,20 +5,20 @@ clearvars % Clear workspace variables
 clf % Close all figures
 set(0,'DefaultFigureWindowStyle','docked') % Docking figure window
 %% Setting Token Initial Positions
-tokenO1 = transl(1.3,0.8,0.37);
-tokenO2 = transl(1.5,0.9,0.37);                   
-tokenO3 = transl(1.5,1.1,0.37);
-tokenO4 = transl(1.3,1.2,0.37);
-tokenX5 = transl(0.75,1.2,0.37);
-tokenX6 = transl(0.55,1.2,0.35);
-tokenX7 = transl(0.55,1,0.35);
-tokenX8 = transl(0.55,0.8,0.35);
-tokenX9 = transl(0.75,0.8,0.35);
+tokenO1 = transl(1.3,0.7,0.37);
+tokenO2 = transl(1.3,0.85,0.37);                   
+tokenO3 = transl(1.3,1.15,0.37);
+tokenO4 = transl(1.3,1.3,0.37);
+tokenX5 = transl(0.75,1.3,0.37);
+tokenX6 = transl(0.75,1.15,0.35);
+tokenX7 = transl(0.75,0.85,0.35);
+tokenX8 = transl(0.75,0.7,0.35);
+tokenX9 = transl(0.55,1,0.35);
 tokens = {tokenO1 tokenO2 tokenO3 tokenO4 tokenX5 tokenX6 tokenX7 tokenX8 tokenX9}; % Storing token locations
 %% Creating Environment
 Environment(); % Call environment function
 %% Plotting Dobots
-q0 = [0,0,0,0,0]; % Default pose
+qr = deg2rad([0,-42.5,-50,0,0]); % Ready pose
 workspace = [0 2 0 2 0 1]; % Workspace dimensions
 dobot1 = Dobot1;
 hold on;
@@ -40,50 +40,41 @@ for i = 1:1:9 % Plotting 9 Tokens
     drawnow; % Update simulation
     pause(0.001); % Wait before execution
 end
-%% Dobot Operation
-disp('Press <Enter> to begin Dobot gameplay'); % Wait for user input
+%% Dual Dobot Operation
+input('Press <Enter> to begin Dobot gameplay'); % Wait for user input
 
-%Moving Dobot to Token
-% 
-% steps = 80;                             % Number of steps used in each motion
-% 
-% q2 = UR3.ikine(P1,q,[1,1,1,0,0,0]);     % Computing joint angles required for end effector to reach
-% qMatrix = jtraj(q,q2,steps);           % desired position using inverse kinematics
-% x = UR3.fkine(q2);                     % Calculating trajectory required
-% disp(x);                                % using quintic polynomial method
-%                                         % from Lab4 and storing in qMatrix                                                                         
-% for i = 1:6:steps                       % Plotting trajectory for UR3 robot
-%    UR3.plot(qMatrix(i,:));
+% steps = 70; % Setting speed of both robots
+% gripOffset = 0.2; % Setting offset for gripper
+% oldQ = [-0.4 0 0 0 pi 0 0];   % Setting initial pose
+% for i = 1:1:6   % Collecting and stacking bricks
+%     brickTr = bricks{i}*transl(0,0,-gripOffset);   % Calculating pickup point
+%     wallTr = wall{i}*transl(0,0,-gripOffset);   % Calculating drop off point
+%     newQ = ur5.model.ikcon(brickTr,oldQ); % Calculating required pose
+%     newTr = ur5.model.fkine(newQ);   % Calculating forward transform
+%     if abs(brickTr(1,4) - newTr(1,4)) <= 0.1 && abs(brickTr(2,4) - newTr(2,4)) <= 0.1 && abs(brickTr(3,4) - newTr(3,4)) <= 0.1 % Checking brick reachability
+%        disp(['brick',num2str(i),' is within UR5 reach. Calculated transform is:']);  % Display message
+%        disp(newTr); % Display message
+%        if newTr(3,4) >= 0   % Check to see if the transform is above floor
+%           ur5.model.plot(jtraj(oldQ,newQ,steps),'workspace',workSpace); % Move robot to brick
+%           oldQ = newQ; % Update robot pose
+%           newQ = ur5.model.ikcon(wallTr,oldQ); % Calculating required pose
+%           trajectory = jtraj(oldQ,newQ,steps);  % Calculating trajectory
+%           for j = 1:1:steps % Moving robot with brick to wall
+%               ur5.model.animate(trajectory(j,:))    % Update robot pose
+%               transformedVertices=[brickVertices{i},ones(size(brickVertices{i},1),1)]*(ur5.model.fkine(trajectory(j,:))*transl(0,0,gripOffset))'; % Moving brick 
+%               set(bricks_h{i},'Vertices',transformedVertices(:,1:3)); % Updating brick location
+%               drawnow;  % Update simulation
+%           end
+%           oldQ = newQ; % Update robot pose
+%        else
+%            warning(['A pose for brick',num2str(i),' is not possible.']);   % Display message
+%        end
+%     else
+%        warning(['brick',num2str(i),' is out of UR5 reach.']);   % Display message
+%     end
 % end
-% 
-% %% Moving Dobot to Token 2
-% 
-% q3 = UR3.ikine(transl(1,1.1,0.5),q,[1,1,1,0,0,0]);    
-% qMatrix = jtraj(q2,q3,steps);           % Calculating joint angles needed to get to wall, masking off R,P,Y
-%                                         % Working out trajectory from brick
-%                                         % to wall using quintic polynomial
-%                                         % method
-% for i = 1:6:steps
-%    UR3.plot(qMatrix(i,:));              % Plotting movement of UR3 
-%    T2 = UR3.fkine(qMatrix(i,:));        % Using forward kinematics to find and store position of pose of wall
-%    Token.base = T2;                     % Updating brick location to that wall position
-%    Token.plot(0);                       % Plotting movement of brick to move with UR3 end effector
-% end
-% 
-% %% Moving Dobot to Token 1
-% 
-% steps = 80;                             % Number of steps used in each motion
-% 
-% q2 = UR3.ikine(P1,q,[1,1,1,0,0,0]);     % Computing joint angles required for end effector to reach
-% qMatrix = jtraj(q,q2,steps);           % desired position using inverse kinematics
-% x = UR3.fkine(q2);                     % Calculating trajectory required
-% disp(x);                                % using quintic polynomial method
-%                                         % from Lab4 and storing in qMatrix                                                                         
-% for i = 1:6:steps                       % Plotting trajectory for UR3 robot
-%    UR3.plot(qMatrix(i,:));
-% end
+% ur5.model.plot(jtraj(ur5.model.getpos,[-0.4 0 0 0 0 0 0],steps),'workspace',workSpace); % Returning to ready pose
 
-%% Moving Dobot to Token 2
 
 
 
